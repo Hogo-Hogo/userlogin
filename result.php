411cfbfbac83b1dcef0ee1cc1dfc021dbff31b43
Hogo-Hogo <84867312+Hogo-Hogo@users.noreply.github.com>
2021-05-26 19:13:37 +0200

    search.php
    Search full name of user.

---
+++ result.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Search engine of users </title>
+  </head>
+<body>
+  <form action="userpage.php" method="GET">
+    <table>
+        <tr><td>Username to search:</td><td><input type="text" id="username" name="username"></td></tr>
+        <tr><td></td><td><button type="submit">Search</button></td></tr>
+    </table>
+</form>
