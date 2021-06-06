6834f8d4c6565e0b5359c1aa9e8cc9c3c4304aa8
Hogo-Hogo <84867312+Hogo-Hogo@users.noreply.github.com>
2021-06-06 11:31:56 +0200

    Update result.php

--- result.php
+++ result.php
@@ -6,7 +6,7 @@
 <body>
   <form action="userpage.php" method="GET">
     <table>
-        <tr><td>Username to search:</td><td><input type="text" id="username" name="username"></td></tr>
+        <tr><td>Username to search:</td><td><input type="text" id="username" list="user" name="username"></td></tr>
         <tr><td></td><td><button type="submit">Search</button></td></tr>
     </table> 
 </form> 
