64b61b60b6a5e196062ee8dcc0b08c85a97c44bb
Hogo-Hogo <84867312+Hogo-Hogo@users.noreply.github.com>
2021-06-06 11:31:26 +0200

    Update result.php

--- result.php
+++ result.php
@@ -8,5 +8,32 @@
     <table>
         <tr><td>Username to search:</td><td><input type="text" id="username" name="username"></td></tr>
         <tr><td></td><td><button type="submit">Search</button></td></tr>
-    </table>
-</form>
+    </table> 
+</form> 
+  <!-- Datalist from search -->
+<datalist id="user">
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "my_users";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Chyba: " . $conn->connect_error);
+}
+
+$sql = "SELECT username FROM users ORDER BY created_at";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo '<option value="' .  $row['username'] . '">
+    </option>';
+  }
+} else {
+  echo "0 results";
+}
+$conn->close(); 
+?>
+</datalist>
